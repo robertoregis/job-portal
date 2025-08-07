a728705fabe41d602091cdf98ef92b81754a3b7f
robertoregis <robertoppvim@hotmail.com>
2025-08-07 00:42:53 -0300

    fee

--- plugins/supabase.client.ts
+++ plugins/supabase.client.ts
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 export default defineNuxtPlugin(() => {
   const SUPABASE_URL = useRuntimeConfig().public.SUPABASE_URL
   const SUPABASE_ANON_KEY = useRuntimeConfig().public.SUPABASE_ANON_KEY
-  console.log(SUPABASE_URL)
-  console.log(SUPABASE_ANON_KEY)
+  //console.log(SUPABASE_URL)
+  //console.log(SUPABASE_ANON_KEY)
 
   const supabase = createClient(
     SUPABASE_URL as string,
--- server/utils/supabase.ts
+++ server/utils/supabase.ts
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = useRuntimeConfig().public.SUPABASE_URL
 const SUPABASE_SERVICE_ROLE_KEY = useRuntimeConfig().SUPABASE_SERVICE_ROLE_KEY
-console.log(SUPABASE_URL)
-console.log(SUPABASE_SERVICE_ROLE_KEY)
+//console.log(SUPABASE_URL)
+//console.log(SUPABASE_SERVICE_ROLE_KEY)
 
 const supabase = createClient(
   SUPABASE_URL!,
